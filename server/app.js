97a49afb514e9faa5a48c6ecb14f2e878a6c98ff
insightbox-analytics <insightbox.analytics@gmail.com>
2021-12-09 22:57:47 +1100

    initial

--- server/app.js
+++ server/app.js
@@ -9,14 +9,6 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -29,6 +21,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
