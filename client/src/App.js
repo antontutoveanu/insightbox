50613b08ac6bc20f10250c0cc87ceed28c9eb0f1
antontutoveanu <anton.tutoveanu@gmail.com>
2021-12-31 00:45:10 +1100

    navigate

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles/App.css";
 
 // import modules
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Navigate } from "react-router-dom";
 import { createClient, Provider } from "urql";
 
 // import theme
