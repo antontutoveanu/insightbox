195aab9c7fb4aaceacbb0bdc7dff8377bbe06c18
antontutoveanu <anton.tutoveanu@gmail.com>
2021-12-31 21:00:00 +1100

    catch all routing

--- server/main.go
+++ server/main.go
@@ -10,6 +10,9 @@ func main() {
 	r := gin.Default()
 	// Dont worry about this line just yet, it will make sense in the Dockerise bit!
 	r.Use(static.Serve("/", static.LocalFile("./web", true)))
+	r.Use(static.Serve("/about", static.LocalFile("./web", true)))
+	r.Use(static.Serve("/privacypolicy", static.LocalFile("./web", true)))
+	r.Use(static.Serve("/termsofuse", static.LocalFile("./web", true)))
 	api := r.Group("/api")
 	api.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
